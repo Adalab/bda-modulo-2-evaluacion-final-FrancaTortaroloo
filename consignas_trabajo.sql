953c4e02d1cd999e76b2b1113e0eb22671d1dc43
Franca Tortarolo <francantortarolo@gmail.com>
2024-05-18 09:53:46 +0200

    answers 1 - 5

--- consignas_trabajo.sql
+++ consignas_trabajo.sql
@@ -1,23 +1,56 @@
+use sakila;
+
 /*
 1. Selecciona todos los nombres de las películas sin que aparezcan duplicados.
 */
+
+
+SELECT DISTINCT title
+FROM film;
+
 
 /*
 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
 */
 
 
+SELECT title, 
+rating AS classification
+FROM film
+WHERE rating = 'PG-13';
+
+
 /*
 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
 */
+
+
+SELECT title,
+description
+FROM film
+WHERE description IN ('amazing');
+
 
 /*
 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
 */
 
+
+SELECT title, 
+length
+FROM film
+WHERE length > 120;
+
+
 /*
 5. Recupera los nombres de todos los actores.
 */
+
+SELECT first_name, 
+last_name
+FROM actor;
+
+
 
 /*
 6. Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido.
