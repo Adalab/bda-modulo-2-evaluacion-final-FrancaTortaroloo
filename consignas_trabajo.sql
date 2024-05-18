6506087b22e9dc6e131bc894b164d15df5ac2e91
Franca Tortarolo <francantortarolo@gmail.com>
2024-05-18 12:04:44 +0200

    update excercises

--- consignas_trabajo.sql
+++ consignas_trabajo.sql
@@ -101,8 +101,6 @@ GROUP BY rating;
 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.
 */
 
-# customer: customer_id, first_name, last_name
-# rental: customer_id, 
 
 SELECT customer.customer_id, 
 customer.first_name,
@@ -119,57 +117,153 @@ GROUP BY customer_id;
 */
 
 
-
+SELECT
+category.name AS name,
+COUNT(rental.rental_id) AS total_rented
+FROM category
+JOIN film_category
+ON category.category_id = film_category.category_id
+JOIN inventory
+ON film_category.film_id = inventory.film_id
+JOIN rental
+ON inventory.inventory_id = rental.inventory_id
+GROUP BY category.name; 
 
 
 /*
 12. Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y muestra la clasificación junto con el promedio de duración.
 */
 
+
+SELECT rating AS classification,
+AVG(length) AS Average_length
+FROM film
+GROUP BY rating;
+
+
 /*
 13. Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
 */
+
+
+SELECT
+actor.first_name,
+actor.last_name,
+film.title 
+FROM actor
+JOIN film_actor
+ON actor.actor_id = film_actor.actor_id
+JOIN film
+ON film.film_id = film_actor.film_id
+WHERE film.title = 'Indian Love';
+
 
 /*
 14. Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
 */
 
+
+SELECT title,
+description
+FROM film
+WHERE description LIKE '%dog%' or description LIKE '%cat%';
+
+
 /*
 15. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.
 */
+
+
+#film_actor: actor_id, film_id
+# film: film_id
+#actor: actor_id, firt_name, last_name
+
+
+
+
+
 
 /*
 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.
 */
 
+
+SELECT title,
+release_year
+FROM film
+WHERE release_year BETWEEN 2005 AND 2010;
+
+
 /*
 17. Encuentra el título de todas las películas que son de la misma categoría que "Family".
 */
+
+
+
+
+
 /*
 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.
 */
+
+
+
+
+
 /*
 19. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film.
 */
+
+
+
+
+
 /*
 20. Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y muestra el nombre de la categoría junto con el promedio de duración.
 */
+
+
+
+
+
 /*
 21. Encuentra los actores que han actuado en al menos 5 películas y muestra el nombre del actor junto con la cantidad de películas en las que han actuado.
 */
+
+
+
+
+
 /*
 22. Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. Utiliza una subconsulta para encontrar los rental_ids con una duración superior a 5 días y luego selecciona las películas correspondientes.
 */
+
+
+
+
+
 /*
 23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror". Utiliza una subconsulta para encontrar los actores que han actuado en películas de la categoría "Horror" y luego exclúyelos de la lista de actores.
 */
+
+
+
+
 
 /*
 24. BONUS: Encuentra el título de las películas que son comedias y tienen una duración mayor
 a 180 minutos en la tabla film.
 */
 
-/*
-25. BONUS: Encuentra todos los actores que han actuado juntos en al menos una película. La consulta debe mostrar el nombre y apellido de los actores y el número de películas en las que han actuado juntos.
 
+
+
+
+/*
+25. BONUS: Encuentra todos los actores que han actuado juntos en al menos una película. 
+La consulta debe mostrar el nombre y apellido de los actores y el número de películas en las que han actuado juntos.
 */
+
+
+
+
